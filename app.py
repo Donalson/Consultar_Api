e7bf91c59cd8c0da6fd18eacaa98df38bc763224
Donalson Javier <donalson1517@gmail.com>
2022-06-02 13:37:32 -0600

    Version de prueba
    Se elimino la intergracion de axios y se opto por utilizar fetch para realizar la funcion, ya que se utilizaron alrededor de 6 horas con axios y no se consiguio el resultado deseado

--- app.py
+++ app.py
@@ -1,29 +1,33 @@
-#Importacion de flask para crear y ejecutar sitio web, renderizar plantillas y numpy para generar numeros aleatorios
+#Importacion de flask para crear y ejecutar sitio web, renderizar plantillas, numpy para generar numeros aleatorios
+# y flask_cors para utilizar datos externos
 from flask import Flask, render_template
 from numpy import random
+from flask_cors import CORS
 
 #Instanciacion de flask dentro de la variable app
 app = Flask(__name__)
-
-#Se crea una lista para guardar los 6 identificadores de los pokemons
-verificador = []
-#Se añade un valor inicial a la lista
-verificador.append(random.randint(155))
-
-#Se crea un bucle para asegurar de que hay 6 elementos en lista y que sean diferente
-while len(verificador) < 6:
-    aleatorio = random.randint(155)
-    rango = len(verificador)
-    sirve = True
-    for index in range(0,rango):
-        if aleatorio == verificador[index]:
-            diferente = False
-    if sirve:
-        verificador.append(aleatorio)
+CORS(app)
 
 #Creacion de la ruta principal del sitio web y texto de prueba
 @app.route('/')
 def Inicio():
+    #Se crea una lista para guardar los 6 identificadores de los pokemons
+    verificador = []
+    #Se añade un valor inicial a la lista
+    verificador.append(random.randint(151))
+
+    #Se crea un bucle para asegurar de que hay 6 elementos en lista y que sean diferente
+    while len(verificador) < 6:
+        aleatorio = random.randint(151)
+        rango = len(verificador)
+        sirve = True
+        for index in range(0,rango):
+            if aleatorio == verificador[index]:
+                sirve = False
+            if aleatorio == 0:
+                sirve = False
+        if sirve == True:
+            verificador.append(aleatorio)
     return render_template('index.html', lista = verificador)
 
 #Inializacion del sitio web para correr(modo debug encendido para detectar cambios)
---
+++ test.py
@@ -0,0 +1,28 @@
+from numpy import random
+import requests 
+
+#Se crea una lista para guardar los 6 identificadores de los pokemons
+verificador = []
+#Se añade un valor inicial a la lista
+verificador.append(random.randint(151))
+
+#Se crea un bucle para asegurar de que hay 6 elementos en lista y que sean diferente
+while len(verificador) < 6:
+    aleatorio = random.randint(151)
+    rango = len(verificador)
+    sirve = True
+    for index in range(0,rango):
+        if aleatorio == verificador[index]:
+            diferente = False
+    if sirve:
+        verificador.append(aleatorio)
+
+pokemons = []
+for i in range(0,5):
+    url = 'https://pokeapi.co/api/v2/pokemon/'+str(verificador[i])
+    temp = requests.get(url)
+    temp = temp.json()
+    pokemons.append(temp)
+
+#print('name' in pokemons[0])
+#print(pokemons[0].name)
